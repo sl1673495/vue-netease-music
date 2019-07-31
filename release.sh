fe4404c8525273c4d6ed2c70d0a87b096813745a
sl1673495 <949589855@qq.com>
2019-08-01 06:13:41 +0800

    feat: 优化自动发布命令交互

--- release.sh
+++ release.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
- 
+
+echoCommon()
+{
+  echo -e "\033[33m $1 \033[0m"
+}
+
+echoFail()
+{
+  echo -e "\033[31m $1 \033[0m"
+}
+
+echoSuccess()
+{
+  echo -e "\033[32m $1 \033[0m"
+}
+
 git add .
-echo 'git add success'
+echoSuccess 'git add success'
 echo
-echo "输入commit message"
+echoCommon "输入commit message"
 read message
 git commit -m "$message"
-echo 'commit success'
+echoSuccess 'commit success'
 echo
-echo "选择要发布的方式?"
+echoCommon "选择要发布的方式?"
 select var in release-major release-minor release-patch;
 do
     break
@@ -16,5 +31,9 @@ done
  
 npm run $var
 
-echo "release success"
-git push --follow-tags origin master
+if [ $? -eq 0 ]; then
+    echoSuccess "release success"
+    git push --follow-tags origin master
+else
+    echoFail "release failed"
+fi
