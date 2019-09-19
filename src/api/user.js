9ba4804d9e23f682e0ea1d5cb1b0747360883618
ssh <949589855@qq.com>
2019-09-19 14:50:47 +0800

    fix: 登录接口取消代理

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 import { requestWithoutLoading } from '@/utils'
 
-export const getUserDetail = (uid) => requestWithoutLoading.get("/user/detail", { params: { uid, proxy: 'http://111.231.140.109:8888' } })
+export const getUserDetail = (uid) => requestWithoutLoading.get("/user/detail", { params: { uid } })
 
 const PLAYLIST_LIMIT = 1000
 export const getUserPlaylist = (uid) => requestWithoutLoading.get("/user/playlist", { params: { uid, limit: PLAYLIST_LIMIT } })
